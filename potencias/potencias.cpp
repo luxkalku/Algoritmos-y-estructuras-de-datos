ee31953437b97cbe872a3956c9a6e5412106211f
luxkalku <jerevelasco@hotmail.com>
2017-04-27 11:50:21 -0300

    algoritmos

--- potencias/potencias.cpp
+++ potencias/potencias.cpp
@@ -10,13 +10,8 @@ main()
 	i = 1;
 	cout<<"ingrese una base"<<endl;
 	cin>>base;	
-	cout<<"ingrese un exponente mayor o igual a 0"<<endl;
+	cout<<"ingrese un exponente"<<endl;
 	cin>>n;
-	while (n<0)
-	{
-		cout<<"ingrese un exponente mayor o igual a 0";
-		cin>>n;
-	}
 	
 	double resultado = pow(base,n);
 	
