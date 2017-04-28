76535f619867d7dab84679c0674b75761ade7afb
luxkalku <jerevelasco@hotmail.com>
2017-04-28 14:03:47 -0300

    algoritmos

---
+++ precision/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+ float numero;
+
+ cout << "Escribe un numero real"<<endl;
+ cin >> numero;
+
+
+ cout.precision(1);
+ cout << numero << endl;
+
+ cout.precision(2);
+ cout << numero << endl;
+
+ cout.precision(3);
+ cout << numero << endl;
+
+ cout.precision(4);
+ cout << numero << endl;
+
+ cout.precision(5);
+ cout << numero << endl;
+
+ cout.precision(6);
+ cout << numero << endl;
+
+ cout.precision(7);
+ cout << numero << endl;
+
+ cout.precision(8);
+ cout << numero << endl;
+
+ cout.precision(9);
+ cout << numero << endl;
+ 
+ system("PAUSE");
+ return 0;
+}
