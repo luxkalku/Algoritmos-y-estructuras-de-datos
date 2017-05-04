519dbdb6def99f2ca011f3ce6e71cb99af3d352a
luxkalku <jerevelasco@hotmail.com>
2017-05-04 20:05:00 -0300

    algoritmos

---
+++ funcion-cuadratica/funcion-piso.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdlib.h>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	float n;
+	int piso;
+	cout<<"ingrese un valor real"<<endl;
+	cin>> n;
+//	piso = floor()
+	cout<< "El valor de la funcion entera de tipo funcion piso para "<<n<< " es "<< floor(n);
+	return 0;
+}
